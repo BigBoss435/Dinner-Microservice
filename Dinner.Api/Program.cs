05453d07770a1878aaeb359290f9cab12dffaa60
Deividas Kalvelis <80637452+BigBoss435@users.noreply.github.com>
2024-07-11 22:49:51 +0300

    Adding authentication, requests

--- Dinner.Api/Program.cs
+++ Dinner.Api/Program.cs
@@ -1,5 +1,11 @@
+using Dinner.Application;
+using Dinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+    .AddApplication()
+    .AddInfrastructure();
     builder.Services.AddControllers();
 }
 var app = builder.Build();
